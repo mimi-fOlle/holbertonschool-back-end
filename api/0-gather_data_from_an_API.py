77984cc4994331764ba0efe61c16a830d4af6a95
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-12-01 14:13:50 +0100

    checker wired

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,28 +8,27 @@ if __name__ == "__main__":
     response = get('https://jsonplaceholder.typicode.com/todos/')
     data = response.json()
     completed = 0
-#    uncompleted = 0
+    uncompleted = 0
     total = 0
     tasks = []
     response2 = get('https://jsonplaceholder.typicode.com/users/')
     data2 = response2.json()
 
-    for i in data2:
-        if i.get('id') == int(argv[1]):
-            employee = i.get('name')
+    for idx in data2:
+        if idx.get("id") == int(argv[1]):
+            employee = idx.get("name")
 
-    for i in data:
-        if i.get('userId') == int(argv[1]):
-            total += 1
-            if i.get('completed') is True:
+    for idx in data:
+        if idx.get("userId") == int(argv[1]):
+            if idx.get("completed") is True:
                 completed += 1
-                tasks.append(i.get('title'))
-#            else:
-#                uncompleted += 1
-#        total = completed + uncompleted
+                tasks.append(idx.get("title"))
+            else:
+                uncompleted += 1
+        total = completed + uncompleted
 
     print("Employee {} is done with task({}/{}):".format(employee,
                                                          completed, total))
 
-    for i in tasks:
-        print("\t {}".format(i))
+    for idx in tasks:
+        print("\t {}".format(idx))
