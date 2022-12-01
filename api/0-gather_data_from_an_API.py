a2b75bbd33018a0aa981413218681ef6dbde97b0
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-12-01 08:48:05 +0100

    declarer uncomplete

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     response1 = requests.get('https://jsonplaceholder.typicode.com/todos/')
     data1 = response1.json()
     completed = 0
+    uncompleted = 0
     total = 0
     tasks = []
     response2 = requests.get('https://jsonplaceholder.typicode.com/users/')
@@ -20,11 +21,14 @@ if __name__ == "__main__":
 
     for idx in data1:
         if idx.get("userId") == int(argv[1]):
-            total += 1
 
             if idx.get("completed") is True:
                 completed += 1
                 tasks.append(idx.get("title"))
+            else:
+                uncompleted += 1
+        total = completed + uncompleted
+
 
     print("Employee {} is done with task({}/{}):".format(employee,
                                                          completed, total))
