0c2e9fedbecc2d4728ec75f58615a0703a909617
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-12-01 08:22:10 +0100

    get from api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Returns info about his/her TODO list progress by giving employee ID"""
+from sys import argv
+import json
+import requests
+
+
+if __name__ == "__main__":
+    response1 = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    data1 = response1.json()
+    completed = 0
+    total = 0
+    tasks = []
+    response2 = requests.get('https://jsonplaceholder.typicode.com/users/')
+    data2 = response2.json()
+
+    for idx in data2:
+        if idx.get("id") == int(argv[1]):
+            employee = idx.get("name")
+
+    for idx in data1:
+        if idx.get("userId") == int(argv[1]):
+            total += 1
+
+            if idx.get("completed") is True:
+                completed += 1
+                tasks.append(idx.get("title"))
+
+    print("Employee {} is done with task({}/{}):".format(employee,
+                                                         completed, total))
+
+    for idx in tasks:
+        print("\t {}".format(idx))
