668a5f855115aaf3e9ebe3b3f2aa0d50601af8f4
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-12-02 14:04:00 +0100

    remettre

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
-'''
-Python script that returns information using REST API
-'''
-import requests
+"""Returns info about his/her TODO list progress by giving employee ID"""
+from requests import get
 from sys import argv
 
+
 if __name__ == "__main__":
-    if len(argv) > 1:
-        user = argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
-        req = requests.get("{}users/{}".format(url, user))
-        name = req.json().get("name")
-        if name is not None:
-            jreq = requests.get(
-                "{}todos?userId={}".format(url, user)).json()
-            alltsk = len(jreq)
-            completedtsk = []
-            for t in jreq:
-                if t.get("completed") is True:
-                    completedtsk.append(t)
-            count = len(completedtsk)
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name, count, alltsk))
-            for title in completedtsk:
-                print("\t {}".format(title.get("title")))
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+    completed = 0
+    uncompleted = 0
+    total = 0
+    tasks = []
+    response2 = get('https://jsonplaceholder.typicode.com/users/')
+    data2 = response2.json()
+
+    for idx in data2:
+        if idx.get("id") == int(argv[1]):
+            employee = idx.get("name")
+
+    for idx in data:
+        if idx.get("userId") == int(argv[1]):
+            if idx.get("completed") is True:
+                completed += 1
+                tasks.append(idx.get("title"))
+            else:
+                uncompleted += 1
+        total = completed + uncompleted
+
+    print("Employee {} is done with task({}/{}):".format(employee,
+                                                         completed, total))
+
+    for idx in tasks:
+        print("\t {}".format(idx))
